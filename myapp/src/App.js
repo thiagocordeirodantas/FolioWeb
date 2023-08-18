19de033c3c4356e49cd5a28379fe9f8a04b20e7b
Thiago <thiagocoordeirodantas@gmail.com>
2023-08-18 10:59:11 -0300

    Finalizando o header e o conteudo

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
        <Header/>
-       <Main/>
+
     </div>
   );
 }
--- myapp/src/components/Header/index.js
+++ myapp/src/components/Header/index.js
@@ -2,7 +2,7 @@ import './Header.css'
 
 const Header =  () => {
     return (
-       <header>
+       <header className="cabecalho">
            <nav>
                 <ul className="lista-links">
                     <li className="lista-item"><a href="#" className="lista-link-item">Home</a></li>
@@ -11,7 +11,20 @@ const Header =  () => {
                     <li className="lista-item"><a href="#" className="lista-link-item">About</a></li>
                 </ul>
            </nav>
+            <section className="conteudo-container">
+                <section className="conteudo">
+                    <h1 className="mini-card">🤗 Hello All</h1>
+                    <h1 className="titulo-conteudo">I’m Shambhavi, A Product Designer and Problem Solver</h1>
+                    <aside className="botoes">
+                        <a href="#" className="botao-1">View My Work</a>
+                        <a href="#" className="botao-2">Contact Me</a>
+                    </aside>
+                </section>
+                    <img className="imagem" src="imagem-main.png" alt="Imagem do conteudo"/>
+            </section>
+          
        </header>
+       
     )
 }
 
